d4f237239989da64694826826f92002e7af87843
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-06-18 19:39:17 -0400

    bump

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 # auth in .pypirc
 rm -rf ../betaflight-python/dist
+rm -rf ../betaflight-python/betaflight/firmware/obj
+rm -rf ../betaflight-python/betaflight/firmware/tools
+rm -rf ../betaflight-python/betaflight/firmware/downloads
 pip install --upgrade build twine
 python3 -m build --sdist
 python3 -m twine upload dist/*
