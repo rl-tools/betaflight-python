f7618f56d7e0251b4d92dd72a8f1891d851c54b4
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-06-17 21:34:31 -0400

    works well

--- hatch_config.py
+++ hatch_config.py
@@ -1,8 +1,19 @@
+
+from pathlib import Path
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 import subprocess
-import os
 
 class CustomBuildHook(BuildHookInterface):
+    PLUGIN_NAME = "betaflight-sitl"
+
     def initialize(self, version, build_data):
-        server_dir = os.path.join(os.path.dirname(__file__), "betaflight/firmware")
-        subprocess.run(["make", "TARGET=SITL"], cwd=server_dir, check=True)
+        project_root = Path(self.root)
+        firmware_dir = project_root / "betaflight" / "firmware"
+
+        if self.target_name == "sdist":
+            return
+
+        subprocess.run(["make", "TARGET=SITL"], cwd=firmware_dir, check=True)
+        build_data.setdefault("artifacts", []).append(
+            str(firmware_dir / "obj")
+        )
